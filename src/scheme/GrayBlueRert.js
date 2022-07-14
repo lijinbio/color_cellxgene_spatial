c5199a6e3d53548df8b76f7c71d67e6c04f14e34
Jin Li <lijin.abc@gmail.com>
2022-07-14 12:15:57 -0500

    src/scheme/GrayBlueRert.js: rm light blue

--- src/scheme/GrayBlueRert.js
+++ src/scheme/GrayBlueRert.js
@@ -3,13 +3,8 @@ import {interpolateRgbBasis} from "d3-interpolate";
 export var scheme = [
   '#d9d9d9', '#d9d9d9', // grey
   '#d9d9d9', '#d9d9d9',
-  '#d9d9d9', '#d9d9d9',
-  '#d9d9d9', '#d9d9d9',
   '#d9d9d9',
-
-  '#f7fbff', '#deebf7', // blue gradient
-  '#c6dbef', '#9ecae1',
-  '#6baed6', '#4292c6',
+  '#6baed6', '#4292c6', // blue gradient
   '#2171b5', '#08519c',
   '#08306b'
 ];
